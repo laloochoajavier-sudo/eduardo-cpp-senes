2dd65a93f8e44fe19f69d9967a15aff991a1e15c
laloochoajavier-sudo <laloochoajavier@gmail.com>
2026-02-03 21:53:58 -0600

    03-02-26 funciones sobrecargadas

--- funciones_sobrecargadas.cpp
+++ funciones_sobrecargadas.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
-#include <ctime>
-int main(){
 
+void hornearPizza();
+void hornearPizza(std::string ingrediente_dos,std::string ingrediente_uno);
+
+int main(){
+std::string opcion1;
+std::string opcion2;
+std::string v;
+
+std::cout<<"####################\n";
+std::cout<<" pizzeria Eduardos\n";
+std::cout<<"####################\n";
+
+hornearPizza("Peperroni","Mozzarella");
 
     return 0;
+}
+void hornearPizza(){
+    std::cout<<"aqui esta tu pizza\n";
+}
+void hornearPizza(std::string ingrediente_uno){
+    std::cout<<"aqui esta tu pizza de "<<ingrediente_uno<<"\n";
+}
+void hornearPizza(std::string ingrediente_uno,std::string ingrediente_dos){
+    std::cout<<"aqui esta tu pizza de "<<ingrediente_uno<<" y "<<ingrediente_dos<<"\n";
 }
