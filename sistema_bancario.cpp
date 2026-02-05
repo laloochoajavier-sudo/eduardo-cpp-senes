48e8b6e6b3f01844abfefce501c563ff9c6c8ea5
laloochoajavier-sudo <laloochoajavier@gmail.com>
2026-02-04 22:44:24 -0600

    04/02/2026-un banco simple

--- sistema_bancario.cpp
+++ sistema_bancario.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <ctime>
 #include <iomanip>
+#include <limits>
 
-void MostrarSaldo( double saldo );
+void MostrarSaldo(double saldo );
 double deposito (double saldo);
 double retirar (double saldo);
 
@@ -14,12 +15,24 @@ int main(){
     std::cout<<"\n*******************\n";
     std::cout<<"ingrese una opcion:\n";
     std::cout<<"*******************\n";
+    std::cout<<"ingrese dos veces su opcion para confirmar\n";
     
     std::cout<<"1. Mostrar Saldo\n";
     std::cout<<"2. Depositar dinero\n";
     std::cout<<"3. Retirar dinero\n";
-    std::cout<<"4. Salir\n";
+    std::cout<<"4. Salir ";
     std::cin>>opc;
+
+    //esto sirve para que si no ingresa una variable que se solicita vuelva a imprimir el mensaje ejemplo 1 es igual a: p 1 es igual a: 1
+    //limpiador de opciones <limits>
+    if(!(std::cin >> opc)){
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+    std::cout<<"Entrada no valida! Intente de nuevo.";
+    continue;
+    }
+    
+    
     switch (opc)
     {
     case 1:
@@ -35,8 +48,10 @@ int main(){
         break;
     case 4:
         std::cout<<"gracias por usar nuestro banco";
+        break;
     default:
         std::cout<<"seleccione un opcion valida";
+        break;
         
     }
 
@@ -46,7 +61,7 @@ int main(){
     return 0;
 }
 void MostrarSaldo( double saldo ){
-    std::cout<<"su balance es $: "<<std::setprecision (2) <<std::fixed<< saldo<<'\n';
+    std::cout<<"su balance es $: "<<std::setprecision (2) <<std::fixed<< saldo;
 }
 
 double deposito (double saldo){
