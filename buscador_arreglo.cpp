2927c82051d0cd31efc27e30a5da879cbe5efc7c
laloochoajavier-sudo <laloochoajavier@gmail.com>
2026-02-15 19:41:31 -0600

    15/02/26 un menu simple don de indica la comida del dia

--- buscador_arreglo.cpp
+++ buscador_arreglo.cpp
@@ -1,36 +1,51 @@
 #include <iostream>
+#include <limits>
 //las variables se puden cambiar de nombre pero el tipo de dato no se puede cambiar
-int numeroEncontrado(int arreglo[], int tamano, int elemento);
+int numeroEncontrado(std::string ao[], int tamano, std::string elemento);
 
 int main(){
-int numeros []{0,1,2,3,4,5,6,7,8,9,10};
+std::string comida[] = {"", "tacos", "quesadillas", "tortas", "hamburguesas", "pizza"};
 // esto se llama puntero a un arreglo, el nombre del arreglo es un puntero 
-int tamano = sizeof(numeros)/sizeof(int);
+int tamano = sizeof(comida)/sizeof(std::string);
 int indice;
-int miNum;
-std::cout<<"Busqueda de numeros del 1 al 10 favor de usar el tablero numerico: ";
-std::cin>>miNum;
-//no hace falta poner "[0]" en el arreglo numeros porque es un puntero
-indice = numeroEncontrado(numeros, tamano, miNum);
+std::string miEle;
 
-if (indice != -1 && miNum != 0){
-    std::cout<<"el numero "<<miNum<<" se encuentra en el indice "<<indice<<'\n';
+
+
+std::cout<<"----------------------------------------"<<'\n';
+std::cout<<"        MENU DE COMIDAS DEL DIA"<<'\n';
+std::cout<<"----------------------------------------"<<'\n';
+std::cout<<"-TACOS"<<'\n';
+std::cout<<"-QUESADILLAS"<<'\n';
+std::cout<<"-TORTAS"<<'\n';
+std::cout<<"-HAMBURGUESAS"<<'\n';
+std::cout<<"-PIZZA"<<'\n';
+std::cout<<"----------------------------------------"<<'\n';
+std::cout<<"Busqueda de comida en el menu : ";
+std::getline(std::cin, miEle);
+
+//no hace falta poner "[0]" en el arreglo numeros porque es un puntero
+indice = numeroEncontrado(comida, tamano, miEle);
+
+
+if (indice != -1 && miEle != ""){
+    std::cout<<"la comida "<<miEle<<" se encuentra en el indice "<<indice<<'\n';
 }
 
 else{
-    std::cout<<"el numero no se encuentra en el catalogo"<<std::endl;
+    std::cout<<"la comida no se encuentra en el catalogo"<<std::endl;
 }
 
-    indice = numeroEncontrado(numeros, tamano, miNum);
+    indice = numeroEncontrado(comida, tamano, miEle);
 
 
     return 0;
 }
 //indice el cual se rije por el tamaño y usan el arreglo para comparar el elemento que se busca
-//elemento = miNum, el cual se ingresa por el usuario
-int numeroEncontrado(int arreglo[], int tamano, int elemento){
+//elemento = miEle, el cual se ingresa por el usuario
+int numeroEncontrado(std::string comida[], int tamano, std::string elemento){
     for (int i =0; i <tamano; i++){
-        if (arreglo[i] == elemento){
+        if (comida[i] == elemento){
             return i; 
         }
     }return -1;
