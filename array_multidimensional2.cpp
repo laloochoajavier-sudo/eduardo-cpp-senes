7df6dc6f59c40c5441986f51ce3f19b52a51edd9
laloochoajavier-sudo <laloochoajavier@gmail.com>
2026-02-24 16:58:36 -0600

    hi

--- array_multidimensional2.cpp
+++ array_multidimensional2.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 #include <ctime>
 int main(){
- std::string autos[][3]={
+ std::string autos[][5]={
 {"honda","corola","mustang"},
 {"botsvagen","toyota","ferrari"},
 {"bochito","corvett","kia"}
+};
+int filas = sizeof(autos)/sizeof(autos[0]);
+int columnas = sizeof (autos[0])/sizeof (autos[0][0]);
+for(int i = 0; i< filas; i++){
+    for( int j = 0; j< columnas; j++){ 
+        std::cout<<autos[i][j] <<" ";
 }
-int filas = sizeof(autos)/sizeof(autos[0])
-int columnas = sizeof (autos[])/sizeof (autos[0][0])
-for(i = 0; i< filas; i++){
-for(j = 0; j< columnas; j++){
+std::cout<<'\n';       
 }
-std::cout<comidas[i][j             
-}
-std::cout<<'\n';
 
 
     return 0;
