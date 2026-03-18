891d302ae3de2e7a4ed09669173379f8495bc3c4
laloochoajavier-sudo <laloochoajavier@gmail.com>
2026-03-17 22:47:22 -0600

    typedef sencillo usando mi nombre

--- typedef_and_type_Alises.cpp
+++ typedef_and_type_Alises.cpp
@@ -1,8 +1,9 @@
 #include<iostream>
 #include<vector>
-//typedef std::vector<std::pair<std::string, int>>pair_list_t;
+/*typedef std::vector<std::pair<std::string, int>>pair_list_t;
 //typedef std::string text_t;
-//typedef int numero_t;
+//typedef int numero_t;*/
+
 using text_t = std::string;
 using numero_t = int;
 
